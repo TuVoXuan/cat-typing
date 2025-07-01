6e5625469d026e794b981e35830229a8d110b40d
TuVoXuan <voxuantu1235@gmail.com>
2025-07-01 15:37:30 +0700

    feat: add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,57 +42,68 @@ export default function Home() {
   };
 
   return (
-    <div className="dark bg-primary h-screen w-full py-8">
-      <Header />
-      <div className="flex items-center gap-x-5 m-10 w-fit mx-auto">
-        <div className="dark flex gap-x-4 rounded-md bg-sub-alt text-foreground p-4 w-fit">
-          <label className="text-sub">Word count:</label>
-          {wordCountList.map((count) => (
-            <button
-              key={count}
-              className={cn(
-                "text-base text-sub hover:text-text transition-colors duration-200 cursor-pointer font-medium",
-                count === wordCount && "text-main"
-              )}
-              onClick={() => handleWordCountChange(count)}
-            >
-              {count}
-            </button>
-          ))}
+    <>
+      <div className="xl:hidden dark bg-primary h-screen w-full py-8">
+        <Header />
+        <div className="flex flex-col flex-1 items-center justify-center mt-10 px-3">
+          <h2 className="text-sub">
+            Sorry our web is not support in mobile and tablet
+          </h2>
+          <h2 className="text-sub">Please access website in your laptop</h2>
+        </div>
+      </div>
+      <div className="hidden xl:block dark bg-primary h-screen w-full py-8">
+        <Header />
+        <div className="flex items-center gap-x-5 m-10 w-fit mx-auto">
+          <div className="dark flex gap-x-4 rounded-md bg-sub-alt text-foreground p-4 w-fit">
+            <label className="text-sub">Word count:</label>
+            {wordCountList.map((count) => (
+              <button
+                key={count}
+                className={cn(
+                  "text-base text-sub hover:text-text transition-colors duration-200 cursor-pointer font-medium",
+                  count === wordCount && "text-main"
+                )}
+                onClick={() => handleWordCountChange(count)}
+              >
+                {count}
+              </button>
+            ))}
+          </div>
+
+          <SelectCaretStyle />
         </div>
 
-        <SelectCaretStyle />
-      </div>
+        <ListWords wordStr={wordsList} />
 
-      <ListWords wordStr={wordsList} />
+        <div className="flex w-full justify-center mt-4">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button onClick={handleRestartTest} className="cursor-pointer">
+                <RotateCw className="size-5 text-sub hover:text-text" />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p className="font-medium text-base">Restart Test</p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
 
-      <div className="flex w-full justify-center mt-4">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <button onClick={handleRestartTest} className="cursor-pointer">
-              <RotateCw className="size-5 text-sub hover:text-text" />
-            </button>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p className="font-medium text-base">Restart Test</p>
-          </TooltipContent>
-        </Tooltip>
-      </div>
+        <div className="mt-5 mx-auto flex w-fit">
+          <CatTypeImage />
+        </div>
 
-      <div className="mt-5 mx-auto flex w-fit">
-        <CatTypeImage />
+        <div className="fixed bottom-10 left-1/2 -translate-x-1/2">
+          <a
+            target="blank"
+            href="https://github.com/TuVoXuan/cat-typing"
+            className="flex items-center gap-2 text-sub hover:text-text"
+          >
+            <CodeXml className="size-5 stroke-2" />
+            <span className="font-medium text-sm">github</span>
+          </a>
+        </div>
       </div>
-
-      <div className="fixed bottom-10 left-1/2 -translate-x-1/2">
-        <a
-          target="blank"
-          href="https://github.com/TuVoXuan/cat-typing"
-          className="flex items-center gap-2 text-sub hover:text-text"
-        >
-          <CodeXml className="size-5 stroke-2" />
-          <span className="font-medium text-sm">github</span>
-        </a>
-      </div>
-    </div>
+    </>
   );
 }
